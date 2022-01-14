ef1ba00dc6da4f9d1eff926b282ae2e05fa3bfbf
uuronn <uuronprogram@gmail.com>
2022-01-14 20:32:27 +0900

    add alert

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,37 @@
 import React, { useState } from "react";
 
 export const App = () => {
-  const [incomleteTodos, setIncompleteTodos] = useState(["aaa","iiii"]);
+  const [todoText, setTodoText] = useState("");
+  const [incomleteTodos, setIncompleteTodos] = useState(["a","iiii"]);
+
+  const [compliteTodos, setCompleteTodos] = useState(["uuuu"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    alert(todoText);
+  };
+
   return (
     <>
       <div>
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div>
         <p>未完了のTODO</p>
         <ul>
           {incomleteTodos.map((todo) => {
-          })}
-          <li>
+            return (
+          <li key={ todo }>
+            { todo }
             <p>aaa</p>
-            <button>完了</button>
+            <button>fdfdfddfd</button>
             <button>削除</button>
           </li>
+
+            )
+          })}
           <li>
             <p>iii</p>
             <button>完了</button>
@@ -30,6 +44,12 @@ export const App = () => {
       <div>
         <p>完了のTODO</p>
         <ul>
+          {compliteTodos.map((todo) => {
+            return (
+              <li key={todo}>{todo}
+              <button>もどす</button></li>
+            )
+          })}
           <li>
             <p>aaa</p>
             <button>戻す</button>
